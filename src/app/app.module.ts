f9a55605f72681ac485cdec6b33b36d7b223f6da
Rodrigo Cunha <rodrigo.cunha@manifestohut.com.br>
2022-06-22 01:05:52 -0300

    feat: updatea modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbMenuModule, NbIconModule, NbButtonModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbMenuModule, NbIconModule, NbButtonModule, NbCardModule, NbTreeGridModule, NbInputModule, NbCheckboxModule, NbDialogModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { GamesComponent } from './pages/games/games.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { BooksComponent } from './pages/books/books.component';
+import { BooksEditboxDialog } from './pages/books/books-editbox/books-editbox.component';
 
 @NgModule({
 	declarations: [
@@ -16,6 +17,7 @@ import { BooksComponent } from './pages/books/books.component';
 		GamesComponent,
 		MoviesComponent,
 		BooksComponent,
+		BooksEditboxDialog,
 	],
 	imports: [
 		BrowserModule,
@@ -24,10 +26,15 @@ import { BooksComponent } from './pages/books/books.component';
 		NbThemeModule.forRoot({ name: 'dark' }),
 		NbSidebarModule.forRoot(),
 		NbMenuModule.forRoot(),
+		NbDialogModule.forRoot(),
 		NbLayoutModule,
 		NbEvaIconsModule,
 		NbIconModule,
 		NbButtonModule,
+		NbCardModule,
+		NbTreeGridModule,
+		NbInputModule,
+		NbCheckboxModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent]
